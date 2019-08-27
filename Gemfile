c30075f4a214c13511c3606d45732f8cf6ad35b2
Student <example@example.com>
2019-08-27 14:58:38 -0700

    Merge branch 'master' of https://github.com/ShaDuv/Lyrical-Personality-TeamWk2 into emotions

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  # For loading API keys from .env file
+  gem 'dotenv-rails'
 end
 
 group :development do
@@ -48,3 +50,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# For analysing lyrics
+gem 'ibm_watson'
---
+++ lib/personality_traits.rb
@@ -0,0 +1,27 @@
+# In order for this class to work, the following needs to be in .env:
+# PERSONALITY_APIKEY={Your API Key, no brakets}
+
+require("json")
+require("ibm_watson/personality_insights_v3")
+
+class PersonalityTraits
+  attr_reader :percents # Hash for accessing the percents for each personality trait. Keys are the personality traits in all caps: "OPENNESS", Conscientiousness"CONSCIENTIOUSNESS", "EXTRAVERSION", "AGREEABLENESS" and "EMOTIONAL RANGE"
+
+  def initialize(input_string)
+    personality_insights = IBMWatson::PersonalityInsightsV3.new(
+      iam_apikey: ENV['PERSONALITY_APIKEY'],
+      version: "2017-10-13"
+    )
+    profile_result = personality_insights.profile(
+      content: input_string,
+      content_type: "text/plain",
+      accept: "application/json",
+      raw_scores: true,
+      consumption_preferences: true
+    ).result
+    @percents = {}
+    profile_result["personality"].each do |personality_trait|
+      @percents[personality_trait['name'].upcase] = (personality_trait['percentile'] * 100).round(0)
+    end
+  end
+end
