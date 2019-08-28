0b52deea0691c9f4ddd0848e06d0b555cf23df4e
BenB-C <bcauchon@gmail.com>
2019-08-28 14:51:21 -0700

    Revert "Connect search box to controller and display results"
    This reverts commit a87510962ac9a2d10234c8b81b01ddf5d80cb21d.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   get '/personality_test', to: 'tests#personality_traits'
   get '/emotions_test', to: 'tests#emotions'
   root 'welcome#index'
-  get '/get_results', to: 'welcome#get_artist'
 end
--- lib/lyrics.rb
+++ lib/lyrics.rb
@@ -2,44 +2,42 @@ require 'rest_client'
 require 'json'
 require 'mechanize'
 
-class Lyrics
-  # This function takes user input and puts it in an array to be passed into our lyrics_by_artist function. To search with multiple artists the input needs to be seperated by a ","
-  def self.user_input_to_array(user_input_as_string)
-    output_array = user_input_as_string.split(",")
-    output_array.each do |artist|
-      artist.strip!
-      artist.tr!(" ", "-")
-    end
+# This function takes user input and puts it in an array to be passed into our lyrics_by_artist function. To search with multiple artists the input needs to be seperated by a ","
+def user_input(user_input_as_string)
+  output_array = user_input_as_string.split(",")
+  output_array.each do |artist|
+    artist.strip!
+    artist.tr!(" ", "-")
   end
+end
 
-  #This function takes an array of strings as an argument
-  def self.lyrics_by_artist(artist_array)
-    lyrics = ""
+#This function takes an array of strings as an argument
+def lyrics_by_artist(artist_array)
+  lyrics = ""
 
-    if artist_array.any? && artist_array.length <= 1
+  if artist_array.any? && artist_array.length <= 1
 
-      artist_array.each do |artist|
-        response = RestClient::Request.execute(
-          method: :get,
-          url: "https://itunes.apple.com/search?term=#{artist}",
-        )
-        parsed_response = JSON.parse(response)
+    artist_array.each do |artist|
+      response = RestClient::Request.execute(
+        method: :get,
+        url: "https://itunes.apple.com/search?term=#{artist}",
+      )
+      parsed_response = JSON.parse(response)
 
-        10.times do |num|
-          sleep 0.05
-          # name of song
-          track = parsed_response["results"][num]["trackName"].tr(" ", "-")
-          track.tr!(",''", "")
-          # scrape the first result of a google search
-          mechanize = Mechanize.new
-          # go to genius page with lyrics
-          page = mechanize.get("https://genius.com/lyrics/#{artist}/#{track.downcase}")
-          # gather lyrics from page and format for search
-          lyrics += page.search('.referent').children.text.split(/(?=[A-Z])/).join(" ") + " "
-        end
+      10.times do |num|
+        sleep 0.05
+        # name of song
+        track = parsed_response["results"][num]["trackName"].tr(" ", "-")
+        track.tr!(",''", "")
+        # scrape the first result of a google search
+        mechanize = Mechanize.new
+        # go to genius page with lyrics
+        page = mechanize.get("https://genius.com/lyrics/#{artist}/#{track.downcase}")
+        # gather lyrics from page and format for search
+        lyrics += page.search('.referent').children.text.split(/(?=[A-Z])/).join(" ") + " "
       end
     end
-    #return lyrics
-    lyrics
   end
+  #return lyrics
+  lyrics
 end
