ab9b2896fd16bc3713aeb2360961ed9395696d47
Jessica Doss <doss.jessicam@gmail.com>
2019-08-28 14:47:55 -0700

    fix logos and add more content to personality section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,1 @@
-let navbar = $(".navbar");
 
-$(window).scroll(function () {
-  // get the complete hight of window
-  let oTop = $(".section-3").offset().top - window.innerHeight;
-  if ($(window).scrollTop() > oTop) {
-    navbar.addClass("sticky");
-  } else {
-    navbar.removeClass("sticky");
-  }
-});
