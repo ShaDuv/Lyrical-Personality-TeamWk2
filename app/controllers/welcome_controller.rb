f58a8d5e6d01719d847a0a5050757a59bbdaa81a
Student <example@example.com>
2019-08-29 12:26:43 -0700

    Make bars show and change size

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,14 +8,15 @@ class WelcomeController < ApplicationController
   end
 
   def get_results
-    user_input = params[:search]
-    lyrics = Lyrics.lyrics_by_artist(Lyrics.user_input_to_array(user_input))
+    @artist = params[:search]
+    lyrics = Lyrics.lyrics_by_artist(Lyrics.user_input_to_array(@artist))
     @personality_percents = PersonalityTraits.new(lyrics).percents
     emotions = Emotions.new(lyrics)
     @joy = (emotions.calc_joy * 100).round(0)
     @anger = (emotions.calc_anger * 100).round(0)
     @fear = (emotions.calc_fear * 100).round(0)
     @sadness = (emotions.calc_sadness * 100).round(0)
+    @results = true
     render :index
   end
 end
